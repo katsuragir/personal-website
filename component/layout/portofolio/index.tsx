b51ae9b26cb8b48fc8b77c84203c41f5947a85b8
Ridho Bagaskara <ridhobagaskara68@gmail.com>
2022-01-07 08:12:04 +0700

    update4

--- component/layout/portofolio/index.tsx
+++ component/layout/portofolio/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Portofolio() {
   return (
     <section className="project py-5" id="project">
@@ -9,33 +11,61 @@ export default function Portofolio() {
             </div>
 
             <div className="row">
-              <div className="item">
+              <div
+                className="col-4"
+                style={{ height: "100px" }}
+                title="Vapehan"
+              >
                 <div className="project-info">
-                  <img
-                    src="/project/project-1.jpeg"
-                    className="img-fluid"
-                    alt="project image"
-                  />
+                  <a
+                    href="https://vapehan.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Image
+                      src="/project/project-1.jpeg"
+                      className="img-fluid"
+                      alt="project image"
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </a>
                 </div>
               </div>
 
-              <div className="item">
+              <div className="col-4" title="GJSRobots">
                 <div className="project-info">
-                  <img
-                    src="/project/project-2.jpeg"
-                    className="img-fluid"
-                    alt="project image"
-                  />
+                  <a
+                    href="https://gjsrobots.id"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Image
+                      src="/project/project-2.jpeg"
+                      className="img-fluid"
+                      alt="project image"
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </a>
                 </div>
               </div>
 
-              <div className="item">
+              <div className="col-4" title="HSGMoto">
                 <div className="project-info">
-                  <img
-                    src="/project/project-3.jpeg"
-                    className="img-fluid"
-                    alt="project image"
-                  />
+                  <a
+                    href="https://hsgmoto.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Image
+                      src="/project/project-3.jpeg"
+                      className="img-fluid"
+                      alt="project image"
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </a>
                 </div>
               </div>
             </div>
